702e9bc95f49a3aaaec31d6969aef7d0cbae5368
Albert Słotwiński <albertslxx00@gmail.com>
2023-01-23 10:19:21 +0100

    Program updated

--- src/application/IBankAccountsDatabase.cs
+++ src/application/IBankAccountsDatabase.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace CSharpDatabase.Application
 {
-  public interface IBankAccountsDatabase
-  {
-    void Insert(BankAccountModel account);
-    bool Delete(Guid id);
-    bool Update(BankAccountModel account);
-    BankAccountModel Find(Guid id);
-    IEnumerable<BankAccountModel> FindBy(string accNumber, int age);
-  }
+    public interface IBankAccountsDatabase
+    {
+        void Insert(BankAccountModel account);
+        bool Delete(Guid id);
+        bool Update(BankAccountModel account);
+        BankAccountModel Find(Guid id);
+        IEnumerable<BankAccountModel> FindBy(string name, int age);
+    }
 }
 
